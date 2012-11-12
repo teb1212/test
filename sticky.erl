5ce93f43e66e26c1883db261e0abe643c504a552
tomkek <tomasz.rakalski@hotmail.com>
2012-11-12 12:18:33 +0100

    second

---
+++ sticky.erl
@@ -0,0 +1,142 @@
+-module(sticky).
+-export([check_text/2]).
+        
+check_text([$", $b, $a, $n, $d, $n, $a, $m, $e, $", $>|T], Event) -> 
+    take_name(T,[], Event);
+check_text([$", $n, $o, $r, $i, $g, $h, $t, $"|T], Event) -> find_pic(T, Event);
+check_text([$", $c, $a, $l, $-, $d, $a, $t, $u, $m, $"|T], Event) -> 
+    take_date(T, Event);
+check_text([$n, $>, $c, $a, $., $ |T], Event) -> take_time(T, Event);
+check_text([_H|T], Event) -> check_text(T, Event);
+check_text([], [H1, H2, H3, H4|H5]) ->
+       Place = "Sticky Fingers",
+       Adress = "Kaserntorget 7 41118 Göteborg",
+      _Finalevent = [Place, Adress, H1, H2, H3, H4, H5],
+       io:format("~s~n", [Place]).
+
+
+take_name([$<|T], List, [Name|T2])  ->  
+    check_text(T, [lists:reverse([Name|List])|T2]); 
+take_name([H|T], List, Event) ->   take_name(T, [H|List], Event).
+
+
+take_date([$&, $n, $b, $s, $p, $;|T], Event) ->
+ add_date(T,[],Event);
+take_date([_H|T], Event) -> take_date(T, Event); 
+take_date([], _Event) -> ok.
+
+
+add_date([$&, $n, $b, $s, $p, $;|T1], List,
+	 [Name, Description,Time,Date|T2]) -> 
+    add_month(T1, [], [Name, Description, Time, [lists:reverse(List)|Date]|T2]);
+add_date([H|T], List, Event) ->
+	add_date(T, [H|List], Event);
+add_date([], _List, _Event) -> ok.
+
+			
+add_month([$<|T], List, Event) -> format_month(T,lists:reverse(List),Event);
+add_month([H|T], List, Event) -> add_month(T, [H|List], Event); 
+add_month([], _List, _Event) -> ok. 
+
+
+format_month(T1, MList, [Name, Description, Time, Date|T2]) ->
+     {Year, Month, _} = erlang:date(),
+    case MList of
+	"Januari" -> M = 01,
+	 add_year(T1, Year, Month, M, [Name, Description, Time, Date|T2]);
+	"Februari" -> M = 02,
+	 add_year(T1, Year, Month, M, [Name, Description, Time, Date|T2]);
+	"Mars" -> M = 03,
+	 add_year(T1, Year, Month, M, [Name, Description, Time, Date|T2]);
+	"April" -> M = 04,
+	 add_year(T1, Year, Month, M, [Name, Description, Time, Date|T2]);
+	"Maj" -> M = 05,
+	 add_year(T1, Year, Month, M, [Name, Description, Time, Date|T2]);
+	"Juni" -> M = 06,
+	 add_year(T1, Year, Month, M, [Name, Description, Time, Date|T2]);
+	"Juli" -> M = 07,
+	 add_year(T1, Year, Month, M, [Name, Description, Time, Date|T2]);
+	"Augusti" -> M = 08,
+	 add_year(T1, Year, Month, M, [Name, Description, Time, Date|T2]);
+	"September" -> M = 09,
+	 add_year(T1, Year, Month, M, [Name, Description, Time, Date|T2]);
+	"Oktober" -> M = 10,
+	 add_year(T1, Year, Month, M, [Name, Description, Time, Date|T2]);
+	"November" -> M = 11,
+	 add_year(T1, Year, Month, M, [Name, Description, Time, Date|T2]);
+	"December" ->  M = 12,
+         add_year(T1, Year, Month, M, [Name, Description, Time, Date|T2])
+    end.
+
+
+add_year(List, Year, Month, M, [Name, Description, Time, Date|T])
+                                                         when Month > M ->
+    check_text(List,[Name, Description, Time,
+              [integer_to_list(Year+1), $-, integer_to_list(M), $-|Date]|T]);
+add_year(List, Year, _Month, M, [Name, Description, Time, Date|T]) ->
+    check_text(List, [Name, Description, Time, 
+              [integer_to_list(Year), $-, integer_to_list(M), $-|Date]|T]).
+
+	    
+take_time([H1, H2, H3, H4, H5|T1], [Name, Description, Time|T2]) ->
+    check_text(T1, [Name, Description, [H1, H2, H3, H4, H5|Time]|T2]).
+
+
+find_pic([$i, $m, $g, $ , $s, $r, $c, $=, $"|T], Event) ->
+     take_pic(T, [], Event);
+find_pic([_H|T], Event) -> find_pic(T, Event);
+find_pic([], _Event) -> ok. 
+
+
+take_pic([$"|T], List, [Name, Description, Time, Date|Picture]) -> 
+    FullLink = "http://www.stickyfingers.nu/",
+    find_description(T, [Name, Description, Time, Date,
+		   [FullLink ++ lists:reverse(List)|Picture]]);
+take_pic([H|T], List, Event) -> take_pic(T, [H|List], Event).
+
+
+find_description([$<, $p|T], Event) -> description_p(T,[], Event);
+find_description([_H|T], Event) -> find_description(T, Event);
+find_description([], _Event) -> ok. 
+     
+
+description_p([$>,H|T], List, Event) when [H]=/="<" ->
+    take_description_p([H|T], List, Event);
+description_p([$<,$/,$p,$>|T], List, [Name, Description|T2]) ->
+    check_text(T,  [Name, [Description|lists:reverse(List)]|T2]);
+description_p([$<, $b, $r, $ , $/|T], List, Event) ->
+    Space = "\n",
+    description_p(T, [Space|List], Event);
+description_p([_H|T], List, Event) -> description_p(T, List, Event).
+
+
+take_description_p([$<, $b, $r|T], List, Event) -> 
+    Space = "\n",
+    description_p(T, [Space|List], Event);
+take_description_p([$<|T], List, Event) ->
+    description_p([$<|T], List, Event); 
+take_description_p([$&, $a, $m, $p, $;|T], List, Event) -> 
+    take_description_p(T, [$&|List], Event);
+take_description_p([$&, $n, $b, $s, $p, $;|T], List, Event) -> 
+    take_description_p(T, List, Event);
+take_description_p([$&, $a, $r, $i, $n, $g, $;|T], List, Event) -> 
+    take_description_p(T, [229|List], Event);
+take_description_p([$&, $e, $a, $c, $u, $t, $e, $;|T], List, Event) -> 
+    take_description_p(T, [233|List], Event);
+take_description_p([$&, $A, $r, $i, $n, $g, $;|T], List, Event) -> 
+    take_description_p(T, [197|List], Event);
+take_description_p([$&, $a, $u, $m, $l, $;|T], List, Event) -> 
+    take_description_p(T, [228|List], Event);
+take_description_p([$&, $A, $u, $m, $l, $;|T], List, Event) -> 
+    take_description_p(T, [196|List], Event);
+take_description_p([$&, $o, $u, $m, $l, $;|T], List, Event) -> 
+    take_description_p(T, [246|List], Event);
+take_description_p([$&, $O, $u, $m, $l, $;|T], List, Event) -> 
+    take_description_p(T, [214|List], Event);
+take_description_p([$&, $q, $u, $o, $t, $;|T], List, Event) -> 
+    take_description_p(T, [$"|List], Event);
+take_description_p([H|T], List, Event) -> 
+    take_description_p(T, [H|List], Event).
+
+
+
---
+++ sticky_start.erl
@@ -0,0 +1,61 @@
+-module(sticky_start).
+-compile(export_all).
+
+get_info() ->
+
+%Tomasz
+
+    try register(sticky, self())
+    catch
+	error:badarg ->
+	    ok
+    end,
+
+    inets:start(),
+    {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Body}} =
+	httpc:request("http://www.stickyfingers.nu/"),
+    Name = [],
+    Description = [],
+    Time = [],
+    Date = [],
+    Picture = [],
+    Event = [Name, Description, Time, Date, Picture],
+    get_link(Body, Event),
+
+%Tomasz
+    
+    loop().
+
+get_link([$d, $", $>, $<, $a, $ , $h, $r, $e, $f, $=, $"|T], Event) ->
+    take_link(T,[],Event)   ;
+get_link([_H|T], Event) -> get_link(T, Event);
+get_link([], _Event) -> 
+
+%Tomasz
+
+    srv ! {done, self()}.
+
+
+take_link([$"|T], List, Event) ->    
+    NewLink = lists:reverse(List),
+    {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Body}} =
+	httpc:request("http://www.stickyfingers.nu/" ++ NewLink), 
+    sticky:check_text(Body, Event),
+    get_link(T, Event);
+take_link([H|T], List, Event) ->
+    take_link(T, [H|List], Event).
+
+%Tomasz
+
+loop() ->
+    receive
+	{ok, _Pid} ->
+	    io:format("confirmation received~n"),
+	    loop();
+	stop ->
+%	    srv ! {'EXIT', whereis(sticky), shutdown},
+	    ok
+    after 300000 ->
+	    get_info(),
+	    loop()
+    end.
